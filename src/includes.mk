cba8da440e1e92370adebd410fbc70703038c57e
mcilloni <krnlpk@gmail.com>
2014-04-05 16:31:29 +0200

    Fixed freebsd makefile

--- src/includes.mk
+++ src/includes.mk
@@ -2,3 +2,10 @@ INCLUDES := -I$(shell pwd)/../ -I$(shell pwd)/../../deps/
 OBJDIR := $(shell pwd)/../../build
 LIBS := -L$(OBJDIR)
 LDFLAGS := -lcgen -lparse -llex -ltreemap -larray -llist -lutils 
+
+UNAME = $(shell uname -o)
+ifeq ($(UNAME),FreeBSD)
+	LDFLAGS += -lexecinfo
+endif
+
+
